c5737ca44a9dfc6f177262084a8d0e09050249f5
rafaelpleite <==>
2020-12-01 22:47:53 -0300

    att

--- joinpdbqt.py
+++ joinpdbqt.py
@@ -6,8 +6,8 @@ Recebe um diretório com arquivos .pdbqt e retorna um único arquivo .pdbqt de t
 
 import os
 
-
-path = 'C:\SARSCOV2\Favipiravir\FavipiravirPLpro\Teste\Resultado'
+path = 'C:\SARSCOV2\OAII\LuteolinPLProPOOL\LuteolinPLpro\Teste\Resultados'
+limit = float(-7.5)
 
 def main():
     arc, data_load = str(), []
@@ -17,17 +17,25 @@ def main():
                 if names.find('pdbqt') != -1:
                     for i in load(str(path + r'/b'[0] + names)): data_load.append(i)
     for i, d in enumerate(data_load):
-        arc = arc + str('MODEL ') + str(i+1) + d[1:] + '\n'
+        arc = arc + str('MODEL ') + str(i+1) + d[1:]
 
     with open('ligand_out.pdbqt', 'w+') as f:
         f.write(str(arc))
 
 
 def load(i):
-    data = ''
-    with open(i) as fp:
-        data = fp.read()
+    global limit
+    with open(i) as fp: data = fp.read()
     data = data.split('MODEL ')
     data.pop(0)
-    return data
+    arq = []
+    for j, jval in enumerate(data):
+        if float(jval[26:34]) <= limit:
+            arq.append(jval)
+    if len(arq) != 0:
+        return arq
+    else: return ''
+    
+
+
 main()
